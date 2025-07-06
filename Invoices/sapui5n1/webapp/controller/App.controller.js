7fa98958075645815b73758172ebb4fefdeb5cf5
Florencia Luraschi <tilkara17@gmail.com>
2025-07-06 20:46:41 +0000

    Se agregan archivos de la unidad 6 y dos de las 7 los llamados Hello

--- Invoices/sapui5n1/webapp/controller/App.controller.js
+++ Invoices/sapui5n1/webapp/controller/App.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"   
+   // "sap/m/MessageToast"   
 ],
     /**
      * 
@@ -8,19 +8,11 @@ sap.ui.define([
      * @param {typeof sap.m.MessageToast} MessageToast 
      * 
      */
-    function (Controller, MessageToast) {
+    function (Controller) {
         "use strict";
         return Controller.extend("logali.sapui5n1controller.App", {
             onInit: function () {
                 
-            },
-            onShowHello: function () {
-                //read text from i18n model
-                var oBundle = this.getView().getModel("i18n").getResourceBundle();
-                //read property from data model
-                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-                var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-                MessageToast.show("Hello Word");
             }
         });
     });
