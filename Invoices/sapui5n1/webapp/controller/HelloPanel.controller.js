3098631e8cf0436778e9a699fc053b62324782fb
Florencia Luraschi <tilkara17@gmail.com>
2025-07-07 17:46:06 +0000

    Se agrega un fragmento por controlador en la vista HelloPanel sobre el btn como un dialogoque todav no se puede cerrar

--- Invoices/sapui5n1/webapp/controller/HelloPanel.controller.js
+++ Invoices/sapui5n1/webapp/controller/HelloPanel.controller.js
@@ -1,18 +1,19 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"   
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
 ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
      * @param {typeof sap.m.MessageToast} MessageToast 
-     * 
+     * @param {typeof sap.ui.core.Fragment} Fragment
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, Fragment) {
         "use strict";
-        return Controller.extend("logali.sapui5n1controller.HelloPanel", {
+        return Controller.extend("logali.sapui5n1.controller.HelloPanel", {
             onInit: function () {
-                
+
             },
             onShowHello: function () {
                 //read text from i18n model
@@ -21,6 +22,21 @@ sap.ui.define([
                 var sRecipient = this.getView().getModel().getProperty("/recipient/name");
                 var sMsg = oBundle.getText("helloMsg", [sRecipient]);
                 MessageToast.show(sMsg);
+            },
+            onOpenDialog: function () {
+                const oView = this.getView();
+                if (!this.byId("helloDialog")) {
+                    Fragment.load({
+                        id: oView.getId(),
+                        name: "logali.sapui5n1.view.HelloDialog"
+                    }).then(function (oDialog) {
+                        oView.addDependent(oDialog);
+                        oDialog.open();
+                    });
+                } else {
+                    this.byId("helloDialog").open();
+                }
             }
+
         });
     });
