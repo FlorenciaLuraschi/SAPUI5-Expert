4f15a60aa9ff538ca8f547dd973aedbeb285bc71
Florencia Luraschi <tilkara17@gmail.com>
2025-07-08 08:43:17 +0000

    Se creo un objeto gestionado para modular el codigo del dialogo y asi poder reutilizarlo

--- Invoices/sapui5n1/webapp/controller/HelloPanel.controller.js
+++ Invoices/sapui5n1/webapp/controller/HelloPanel.controller.js
@@ -24,24 +24,9 @@ sap.ui.define([
                 MessageToast.show(sMsg);
             },
             onOpenDialog: function () {
-                const oView = this.getView();
-                if (!this.byId("helloDialog")) {
-                    Fragment.load({
-                        id: oView.getId(),
-                        name: "logali.sapui5n1.view.HelloDialog",
-                        controller: this
-                    }).then(function (oDialog) {
-                        oView.addDependent(oDialog);
-                        oDialog.open();
-                    });
-                } else {
-                    this.byId("helloDialog").open();
-                }
+                
             },
-            onCloseDialog: function () {
-                this.byId("helloDialog").close();
-
-            }
+    
 
         });
     });
