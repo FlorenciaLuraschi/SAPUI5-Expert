0793080a295d55dadd9c292581ccfd42c8e01232
Florencia Luraschi <tilkara17@gmail.com>
2025-07-08 22:35:29 +0000

    unidades Binding path agregacion y formateadores estandar

---
+++ Invoices/sapui5n1/webapp/controller/InvoicesList.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+'sap/ui/core/mvc/Controller',
+'sap/ui/model/json/JSONModel'
+],
+/**
+     * 
+     * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel 
+     */
+    function (Controller, JSONModel) {
+    return Controller.extend("logali.sapui5n1.controller.InvoicesList", {
+        onInit: function () {
+            var oViewModel= new JSONModel({
+                usd: "USD",
+                eur:"EUR"
+            });
+            this.getView().setModel(oViewModel,"currency");
+        }
+    });
+});
