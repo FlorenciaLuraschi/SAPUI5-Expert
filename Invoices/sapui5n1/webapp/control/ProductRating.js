543107a4129f94913f2947a87106dd1ce00b06aa
Florencia Luraschi <tilkara17@gmail.com>
2025-07-16 00:17:15 +0000

    Creacion del control personalizado ProductRating

---
+++ Invoices/sapui5n1/webapp/control/ProductRating.js
@@ -0,0 +1,22 @@
+sap.ui.define([
+    'sap/ui/core/control'
+],
+    /**
+     * @param {typeof sap.ui.core.Control} Control
+     */
+    function (Control) {
+        "use strict";
+        return Control.extend("logali.sapui5n1.control.ProductRating", {
+            metadata: {
+
+            },
+
+            init: function () {
+
+            },
+
+            renderer: function (oRn, oControl) {
+
+            }
+        });
+    });
