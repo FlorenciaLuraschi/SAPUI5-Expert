ab97900bc7f51991c2921fd8ce41a0665dce9e10
Florencia Luraschi <tilkara17@gmail.com>
2025-07-16 00:28:39 +0000

    Metadatos

--- Invoices/sapui5n1/webapp/control/ProductRating.js
+++ Invoices/sapui5n1/webapp/control/ProductRating.js
@@ -8,7 +8,35 @@ sap.ui.define([
         "use strict";
         return Control.extend("logali.sapui5n1.control.ProductRating", {
             metadata: {
+                properties: {
+                    value: { type: "float", defaultValue: 0 }
+                },
 
+                aggregations: {
+                    _rating: {
+                        type: "sap.m.RatingIndicator",
+                        multiple: false,
+                        visibility: "hidden"
+                    },
+                    _label: {
+                        type: "sap.m.Label",
+                        multiple: false,
+                        visibility: "hidden"
+                    },
+                    _button: {
+                        type: "sap.m.Button",
+                        multiple: false,
+                        visibility: "hidden"
+                    }
+                },
+
+                events: {
+                    change: {
+                        parameters: {
+                            value: { type: "int" }
+                        }
+                    }
+                }
             },
 
             init: function () {
